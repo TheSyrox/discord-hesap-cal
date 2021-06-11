fd754f48303fe76fab7a5bd398730a844a687f45
boran606 <80204080+boran606@users.noreply.github.com>
2021-06-11 18:16:13 +0300

    Create hesap-çal.sh

---
+++ hesap-çal.sh
@@ -0,0 +1,29 @@
+
+
+echo  opening discord tools...
+
+echo  loading...
+
+echo downloading...
+
+figlet discord tool
+
+apt-get install figlet
+
+read -p 'Username: ' uservar
+
+echo
+
+echo brute force attacking $uservar
+
+echo $uservar$RANDOM
+
+for (( ; ; ))
+
+do
+
+   sleep 1
+
+   echo "$uservar$RANDOM"
+
+done
